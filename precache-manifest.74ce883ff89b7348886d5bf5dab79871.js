c234925614248b448d12013a6d9d978bb7895586
Colby Guan <colbert.guan@salesforce.com>
2020-06-13 16:52:17 -0700

    Updates

---
+++ precache-manifest.74ce883ff89b7348886d5bf5dab79871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79924804635a88ef149f2577ca21990",
+    "url": "/police-brutality-ui/index.html"
+  },
+  {
+    "revision": "e239614bdc0feb871910",
+    "url": "/police-brutality-ui/static/css/main.2d16e38d.chunk.css"
+  },
+  {
+    "revision": "5b14b2c820adf5889834",
+    "url": "/police-brutality-ui/static/js/2.402efd77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/police-brutality-ui/static/js/2.402efd77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e239614bdc0feb871910",
+    "url": "/police-brutality-ui/static/js/main.21133fb3.chunk.js"
+  },
+  {
+    "revision": "a5c3f91d961453c5f8c1",
+    "url": "/police-brutality-ui/static/js/runtime-main.8a80b9d9.js"
+  }
+]);
