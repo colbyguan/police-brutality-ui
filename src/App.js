d9054adc01fa434217c2e6deefa74be5daa6b0dd
Colby Guan <colbert.guan@salesforce.com>
2020-06-22 12:22:41 -0700

    switch to github-built JSON API, compatibility and use epoch int instead of Date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DataFrame from "dataframe-js";
 import './App.css';
-import { getReportsDataFrame } from './Client'
+import { getOfficialAPIDataFrame } from './Client'
 
 const DATE_OPTIONS = { month: 'short', day: 'numeric', year: 'numeric', timeZone: 'UTC'}
 const DATE_FORMAT = new Intl.DateTimeFormat('en-US', DATE_OPTIONS)
@@ -12,6 +12,11 @@ const SIDEBAR_MODES = {
   0: 'Browse all',
   1: 'Browse by date',
   2: 'Browse by state',
+}
+const LINK_STYLES = {
+  'twitter': 'bg-blue-500',
+  'reddit': 'bg-orange-400',
+  'redd': 'bg-orange-400',
 }
 
 function App() {
@@ -27,30 +32,53 @@ function App() {
   const [selectedIncidents, setSelectedIncidents] = useState([])
 
   useEffect(() => {
-    getReportsDataFrame()
+    getOfficialAPIDataFrame()
       .then(df => {
         setAllReports(df)
-        setSelectedDate(getUniqueDateCounts(df)[0][2])
-        setSelectedState(getUniqueStateCounts(df)[0][2])
+        setSelectedDate(getDefaultDate(df));
+        setSelectedState(getDefaultState(df));
       })
       .catch(err => console.error(err))
   }, []);
 
+  const getDefaultDate = df => {
+    const uniqueDates = df.unique('date').transpose().toArray()[0];
+    return uniqueDates.sort((a, b) => a - b)[uniqueDates.length - 1]
+  }
+
+  const getDefaultState = df => {
+    const uniqueStates = df.unique('state').transpose().toArray()[0];
+    return uniqueStates.sort()[0]
+  }
+
   useEffect(() => {
+    setIncidentsToCurrentFilters();
+  }, [sidebarMode,selectedState, selectedDate])
+
+
+  const setIncidentsToCurrentFilters = () => {
     if (allReports === null) {
       return;
     }
     let filtered = [];
-    if (sidebarMode === 1) {
+    if (sidebarMode === 0) {
+      filtered = allReports.toCollection()
+      filtered.sort((a, b) => {
+        if (b.date === a.date) {
+          return a.state.localeCompare(b.state)
+        } else {
+          return (new Date(b.date) - new Date(a.date))
+        }
+      });
+    } else if (sidebarMode === 1) {
       filtered = allReports.filter(row => row.get('date') === selectedDate).toCollection()
       filtered.sort((a, b) => new Date(b.date) - new Date(a.date))
     } else if (sidebarMode === 2) {
       filtered = allReports.filter(row => row.get('state') === selectedState).toCollection()
       filtered.sort((a, b) => new Date(b.date) - new Date(a.date))
     }
-    console.log(filtered);
     setSelectedIncidents(filtered);
-  }, [selectedState, selectedDate])
+  }
 
   const handleSidebarModeClick = (num) => {
     setSidebarMode(parseInt(num))
@@ -86,6 +114,7 @@ function App() {
       .sort((a, b) => a[0].localeCompare(b[0]))
   }
 
+  // Return list of [dateString, count, epochTime] tuples
   const getUniqueDateCounts = (df) => {
     // Unique gets returned as a column; toArray returns as array-of-arrays
     const rawDates = df.unique('date').transpose().toArray()[0];
@@ -93,9 +122,10 @@ function App() {
     rawDates.forEach(d => {
       counts[d] = df.countValue(d, 'date')
     })
-    return Object.keys(counts)
+    const tuples = Object.keys(counts)
+      .map(k => parseInt(k))
       .map(d => [new Date(d), counts[d], d])
-      .sort((a, b) => b[0] - a[0])
+      .sort((a, b) => b[2] - a[2])
       .map(tuple => {
         let formatted;
         if (tuple[0].getFullYear() < 2020) {
@@ -105,6 +135,7 @@ function App() {
         }
         return [formatted, tuple[1], tuple[2]]
       });
+    return tuples;
   }
 
   const isSidebarElemSelected = (key) => {
@@ -113,6 +144,21 @@ function App() {
     } else {
       return key === selectedState
     }
+  }
+
+  const getLinkStyle = (link) => {
+    const url = new URL(link)
+    for (const keyword of Object.keys(LINK_STYLES)) {
+      if (url.host.includes(keyword)) {
+        return LINK_STYLES[keyword]
+      }
+    }
+    return 'bg-gray-400';
+  }
+
+  const getLinkLabel = (link) => {
+    const url = new URL(link)
+    return url.host;
   }
 
   return (
@@ -125,16 +171,19 @@ function App() {
               <div className="flex flex-col items-start mb-4">
                 {Object.keys(SIDEBAR_MODES).map(num => {
                   return <div key={num}
-                              className={`px-2 py-1 rounded-md ${parseInt(num) === parseInt(sidebarMode) ? 'bg-orange-200': ''}`}
+                              className={`cursor-pointer px-2 py-1 rounded-md ${parseInt(num) === parseInt(sidebarMode) ? 'bg-orange-200': ''}`}
                               onClick={() => handleSidebarModeClick(num)}>
                               {SIDEBAR_MODES[num]}</div>
                 })}
               </div>
               <div className="flex flex-col items-start">
                 {getSidebarTuples().map(tuple =>
-                  <div onClick={() => handleClickSidebarElem(tuple[2])} className={`px-2 py-1 rounded-md ${isSidebarElemSelected(tuple[2]) ? 'bg-orange-200' : ''}`} key={tuple[2]}>
+                  <div onClick={() => handleClickSidebarElem(tuple[2])}
+                       className={`cursor-pointer px-2 py-1 rounded-md transition-all ease-linear duration-300 hover:text-black
+                          ${isSidebarElemSelected(tuple[2]) ? 'font-semibold bg-orange-200 text-black' : 'text-gray-600'}`}
+                       key={tuple[2]}>
                     <span>{tuple[0]}</span>
-                    <span className="ml-2 px-1 bg-orange-400 rounded-md">{tuple[1]}</span>
+                    <span className={`ml-2 px-1 rounded-md ${isSidebarElemSelected(tuple[2]) ? 'bg-orange-400' : 'bg-gray-400'}`}>{tuple[1]}</span>
                   </div>)}
               </div>
             </>
@@ -142,15 +191,26 @@ function App() {
         </div>
         <div className="w-3/4">
           {selectedIncidents.map(incident => {
+            if (new Date(incident.date).getFullYear() < 2020) {
+              incident.date = null
+            }
             return (<div key={incident.pb_id} className="max-w-full rounded overflow-hidden border border-gray-400 mb-4">
               <div className="px-6 py-4">
                 <div className="mb-2">
-                  <span className="font-bold text-gray-800 mr-2">{CARD_DATE_FORMAT.format(new Date(incident.date))}</span>
-                  <span className="text-gray-700 mr-1">{incident.city},</span>
-                  <span className="text-gray-600">{incident.state}</span>
+                  <span className="font-bold text-gray-800 mr-2">{!incident.date ? 'Unknown Date' : CARD_DATE_FORMAT.format(new Date(incident.date))}</span>
+                  <span className="uppercase font-semibold text-sm text-gray-700 mr-1">{incident.city},</span>
+                  <span className="uppercase font-semibold text-sm text-gray-600">{incident.state}</span>
+                </div>
+                <div className="mb-2">
+                  <p>{incident.title ? incident.title : incident.name}</p>
                 </div>
                 <div>
-                  <p>{incident.title}</p>
+                  <span className="uppercase font-bold text-gray-600 text-xs mr-2">Sources: </span>
+                  {incident.links.map(link => {
+                    return (<span className={`inline-block rounded-full px-3 py-1 mt-2 font-semibold text-sm mr-2 ${getLinkStyle(link)}`}>
+                      <a target="_blank" rel="noopener noreferrer" href={link}>
+                      {getLinkLabel(link)}</a></span>)
+                  })}
                 </div>
               </div>
             </div>)
--- src/Client.js
+++ src/Client.js
@@ -1,9 +1,10 @@
 import DataFrame from "dataframe-js";
 
-const REQUEST_URL = 'https://api.846policebrutality.com/api/incidents'
+const PHP_API_URL = 'https://api.846policebrutality.com/api/incidents'
+const OFFICIAL_URL = 'https://raw.githubusercontent.com/2020PB/police-brutality/data_build/all-locations.json'
 
-const getReportsDataFrame = () => {
-    return fetch(REQUEST_URL)
+const getPHPAPIDataFrame = () => {
+    return fetch(PHP_API_URL)
     .then(response => response.json())
     .then(json => {
         if (!('data' in json)) {
@@ -15,11 +16,39 @@ const getReportsDataFrame = () => {
         // Hour:min:seconds messes with stuff; API does not appear to have H:M:S info
         df.map(row => {
             const rawDate = new Date(row.get('date'))
-            row.set('date', new Date(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate()))
+            if (!rawDate) {
+                row.set('date', new Date(0));
+            }
+            row = row.set('date', new Date(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate()))
         });
         return df;
     });
 }
 
-export { getReportsDataFrame }
+const getOfficialAPIDataFrame = () => {
+    return fetch(OFFICIAL_URL)
+        .then(response => response.json())
+        .then(json => {
+            if (!('data' in json)) {
+                throw `Missing key 'data' in Github response`
+            }
+            const dataArray = json.data;
+            let df = new DataFrame(dataArray,
+                ['id', 'state', 'city', 'date', 'name', 'links'])
+            // Hour:min:seconds messes with stuff; API does not appear to have accurate H:M:S info
+            df = df.map(row => {
+                const rawDate = new Date(row.get('date'))
+                let newRow;
+                if (!row.get('date')) {
+                    newRow = row.set('date', 946684800000)
+                } else {
+                    newRow = row.set('date', new Date(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate() + 1).getTime() + 25200000);
+                }
+                return newRow;
+            });
+            return df;
+        });
+}
+
+export { getPHPAPIDataFrame, getOfficialAPIDataFrame }
 
